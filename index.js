f4b5ef0ba32ca50f47538ed378e0c11d47fed5f2
Evgeniia Semushina <semajenya96@gmail.com>
2023-01-21 01:00:21 +0200

    add close popup

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
-// найдем элементы
+// найдем элементы для открытия и закрытия попапа
 const EditProfile = document.querySelector('.profile__pen');
 const PopupElement = document.querySelector('.popup');
+const ClosePopupElement = PopupElement.querySelector('.popup__close');
+
 
 
 // Функции
@@ -9,6 +11,9 @@ function OpenedPopup (Element) {
     Element.classList.add('popup_opened');
 }
     
+function ClosePopup (Element) {
+    Element.classList.remove('popup_opened');
+}
 
 
 // События
@@ -16,4 +21,8 @@ function OpenedPopup (Element) {
 EditProfile.addEventListener('click', function() {
     OpenedPopup(PopupElement);
 });
+
+ClosePopupElement.addEventListener('click', function(){
+    ClosePopup(PopupElement);
+})
 
